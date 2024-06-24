11b9a80c7ee8548b7ed2a5d6427a42d0b4a4bd96
Jack Simmonds <jack.sebastion.simmonds@gmail.com>
2024-06-24 13:59:33 +1200

    IDM Model and cleanup

--- Car.py
+++ Car.py
@@ -17,50 +17,6 @@ class Car(object):
         self.position[0] += self.v[0] * dt  # x-position
         self.position[1] += self.v[1] * dt  # y-position
 
-        '''
-        Essentially, the velocity and position directions should always remain in the direction of the path.
-        '''
-
-        '''
-        if path.__class__.__name__ == 'StraightRoad': #works. problem = car's velocity might not go along path. should change.
-        #potential problem is if the car reaches the end of the road, the velocity still accelerates?
-            #check if extension is beyond end of road.
-            self.vx = self.v * np.cos(path.angle) #quite taxing calculations. find better way later.
-            self.vy = self.v * np.sin(path.angle) #--> potentially one-off the second part of the calculation in a different function.
-            x_change = self.vx*dt
-            y_change = self.vy*dt
-            if self.x + x_change < path.end_x or self.y + y_change < path.end_y: #within bounds. might need changing for difference slopes.
-                self.x += x_change
-                self.y += y_change
-            else:
-                #print('self.x = {}, self.y = {}, stopping at end of road.'.format(self.x,self.y))
-                #distance_remaining = etc, then can apply to next path. maybe return?
-                self.x = path.end_x
-                self.y = path.end_y1
-
-        elif path.__class__.__name__ == 'CircleRoad':
-            a = 5
-        '''
-    def check_path(self):
-        # allow error tolerance
-        return
-
-    def check(self): #finish this later. need to work on path-tracking first.
-        relativeDistanceCarInFront = 999
-        relativeVelocityCarInFront = 999
-        relativeAccelerationCarInFront = 999
-        #check for cars in front:
-        minCheck = 5 #let follow distance = 5
-        '''
-        for i in range(len(cars)): #PROBLEM: INCLUDES OWN CAR.
-            if cars[i].identifier != self.identifier:
-                if cars[i].x - self.x > 0:
-
-                #minCheck = min(minCheck, abs(car.x - self.x))
-        '''
-        #return (minCheck)/5
-        #float ranging from 1--> 0. 1 = free to move. 0 = harsh break. 0.5 = start breaking, etc. need to research this. 
-
     def draw(self, screen):
         pygame.draw.circle(screen, 'red', (int(self.position[0]), int(self.position[1])), 25) #last number is radius
 
--- IDM.py
+++ IDM.py
@@ -2,6 +2,9 @@ import numpy as np
 import pygame
 import sys
 import scipy
+from Car import Car
+
+WIDTH, HEIGHT = 800, 600
 
 # Define Model Parameters:
 
@@ -20,7 +23,11 @@ exponent = 4
 # Length of car, set to 1.
 length = 1
 
-# Creating the first car:
+# Creating the system of cars:
+# Car 1: The first car, of which I will apply the ODE model to.
+Car1 = Car(1, 125, 300, 0)
+# Car 2: The car in front, set to have a steady velocity of 30.
+Car2 = Car(2, 125, 300, 30)
 
 
 def find_s_alpha(x_alpha_previous, x_alpha):
@@ -32,10 +39,35 @@ def acceleration(v_alpha, v_alpha_previous, s_alpha):
 
     return dvdt
 
+
+# Set up the display
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Simple Traffic System')
+clock = pygame.time.Clock()
+
 # Tick system:
 while True:
     dt = clock.tick(60) / 1000.0
     screen.fill('WHITE')
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    #Sequence of movement, for 2 cars:
+    Car1.move()
+    attributes = []
+    '''
+    attributes = Car1.get_attributes()
+
+    Car2.acceleration()
+    '''
+
+    # Update the display
+    pygame.display.flip()
+    # Control the frame rate --> 60fps.
+    pygame.time.Clock().tick(60)
 
 
 
